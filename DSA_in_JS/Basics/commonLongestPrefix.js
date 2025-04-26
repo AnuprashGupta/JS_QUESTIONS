dc800e334626c7b1dafc6ce818962150c24a4c50
anuprash gupta <anuprash1850531003@gmail.com>
2025-04-26 13:09:09 +0530

    Update commonLongestPrefix.js

--- DSA_in_JS/Basics/commonLongestPrefix.js
+++ DSA_in_JS/Basics/commonLongestPrefix.js
@@ -9,5 +9,16 @@
 // Output: ""
 
 var longestCommonPrefix = function(strs) {
-    
+    let prefix = strs[0];
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.substring(0, prefix.length - 1);
+            if (prefix === '') {
+                return '';
+            }
+        }
+    }
+    return prefix;  
 }
+
+console.log(longestCommonPrefix(["flower","flow","flight"]));
